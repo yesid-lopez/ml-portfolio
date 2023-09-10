024e11cfb7323ebe3a4664b6985c3dfc17bd23b9
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2023-09-10 13:58:05 +0200

    feat: read projects from sanity

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Project from "./project";
+import { ProjectType } from "@/types/project";
+import { getProjects } from "../../lib/project";
 
 const totalProjects = Array.from(Array(10).keys());
-export default function page() {
+export default async function page() {
+  const projects = await getProjects();
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:md:grid-cols-3 gap-4 px-2">
-      {totalProjects.map((project) => (
-        <Project />
-      ))}
+      {projects &&
+        projects.map((project: ProjectType) => (
+          <Project project={project} key={project.title} />
+        ))}
     </div>
   );
 }
--- app/portfolio/project.tsx
+++ app/portfolio/project.tsx
@@ -1,20 +1,25 @@
-import React from "react";
+import { ProjectType } from "@/types/project";
 import Image from "next/image";
+import sanityClient from "../../lib/sanity.client";
+import { useNextSanityImage } from "next-sanity-image";
+import Link from "next/link";
 
-export default function Project() {
+export default function Project({ project }: { project: ProjectType }) {
+  const coverProps = useNextSanityImage(sanityClient, project.cover);
+
   return (
     <div className="flex flex-col border">
       <div className="flex justify-center">
-        <Image alt="" src={"/project.png"} width={500} height={500} />
+        <Image alt="" src={coverProps.src} width={500} height={500} />
       </div>
       <div className="px-3">
-        <p className="font-bold text-left">Project title</p>
-        <p className="font-light">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          pretium maximus nibh et fringilla. Donec ut maximus purus. Interdum et
-          malesuada fames ac ante ipsum primis in faucibus. Fusce mollis.
-        </p>
-        <button className="rounded-full bg-blue-500 px-3 p-1 font-light text-white">open project</button>
+        <p className="font-bold text-left">{project.title}</p>
+        <p className="font-light">{project.description}</p>
+        <Link href={project.url}>
+          <button className="rounded-full bg-blue-500 px-3 p-1 font-light text-white">
+            open project
+          </button>
+        </Link>
       </div>
     </div>
   );
