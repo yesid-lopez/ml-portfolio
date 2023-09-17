b9e1a73acb9467aaf0ce517958991ad01493600f
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2023-09-17 12:42:26 +0200

    feat: add type animation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,22 +3,29 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="grid md:grid-cols-3 p-5 gap-4 h-[95vh]">
+    <div className="grid md:grid-cols-3 p-10 sm:space-y-10 md:p-5 md:space-x-20 h-full md:h-[95vh]">
       <div className="grid place-items-center">
         <Image src="/hero.png" width={400} height={400} alt=""></Image>
       </div>
       <div className="md:col-span-2">
         <div className="flex flex-col space-y-4 h-full justify-center">
-          <span className="font-medium">Hello World,</span>
-          <span className="text-4xl">I am AI Engineer</span>
-          <span className="font-medium pr-16">
-            I'm skilled in web app development, needs-based testing, and MLOps,
-            including deploying and scaling models. I thrive in environments
-            that foster growth, innovation, and research. Passionate about
-            teaching and leveraging cutting-edge devices for inference when
-            required.
+          <h1 className="mb-2 font-mono text-5xl text-gray-100 md:text-6xl">
+            I&apos;m Yesid <span className="text-3xl md:text-5xl"></span>{" "}
+            <br className="block md:hidden" />
+          </h1>
+          <div className="flex items-center w-max">
+            <h1 className=" w-full animate-typing overflow-hidden whitespace-nowrap border-r-8 border-r-white pr-5 text-4xl text-white font-mono">
+              AI Engineer
+            </h1>
+          </div>
+          <span className="text-xl font-mono md:pr-16">
+            I have experience building web applications and creating needs-based
+            testing strategies. I enjoy working in companies that allow me to
+            grow, learn, innovate and do research. I enjoy teaching others and
+            learning new things. Additionally, I have experience in MLOps
+            deploying models to production, scaling them, testing them, and
+            using cutting-edge devices for inference when it is needed.
           </span>
-          <span>I love cats</span>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 5s steps(14) infinite alternate, blink .7s infinite"
+      }
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
