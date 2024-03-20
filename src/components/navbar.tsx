d009a8403864a8390d4ecfaa282765271916f12e
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2024-03-20 11:02:23 +0100

    fix: update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,17 @@ export function Navbar() {
     {
       name: "Home",
       link: "/",
-      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: <IconHome className="h-4 w-4 text-white" />,
     },
     {
       name: "Journey",
       link: "/journey",
-      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: <IconUser className="h-4 w-4 text-white" />,
+    },
+    {
+      name: "Blog",
+      link: "https://blog.yesidlopez.de",
+      icon: <IconUser className="h-4 w-4 text-white" />,
     },
   ];
 
